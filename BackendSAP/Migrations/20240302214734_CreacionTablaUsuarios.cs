19e51adff1cdf436dc4122ddfd7d7b62c0abf023
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-02 17:48:41 -0400

    migración de la creación de la tabla usuarios hecha

---
+++ BackendSAP/Migrations/20240302214734_CreacionTablaUsuarios.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendSAP.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreacionTablaUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroColegiatura = table.Column<int>(type: "int", nullable: false),
+                    TelefonOficina = table.Column<int>(type: "int", nullable: false),
+                    ImagenPerfil = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    ImagenTitulo = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    DescripcionPsicologo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CalleAv = table.Column<string>(name: "Calle_Av", type: "nvarchar(max)", nullable: false),
+                    Experiencia = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Formacion = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    TipoTerapia = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    CiudadId = table.Column<int>(type: "int", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Ciudades_CiudadId",
+                        column: x => x.CiudadId,
+                        principalTable: "Ciudades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_CiudadId",
+                table: "Usuarios",
+                column: "CiudadId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
