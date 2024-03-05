e5b929bb6537989fbc088b1d18de077abc09832b
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-04 23:20:37 -0400

    DataType changed in users's ID into Scores Model (from int to string).

--- BackendSAP/Modelos/Calificaciones.cs
+++ BackendSAP/Modelos/Calificaciones.cs
@@ -10,8 +10,8 @@ namespace BackendSAP.Modelos
 
         [Required]
         public string observacion { get; set; }
-        public int usuarioId { get; set; } // Clave Foránea del usuario quién hace la calificación
-        public int psicologoId { get; set; } // Clave Foránea del usuario psicólogo quién recibe la calificación
+        public string usuarioId { get; set; } // Clave Foránea del usuario quién hace la calificación
+        public string psicologoId { get; set; } // Clave Foránea del usuario psicólogo quién recibe la calificación
 
         [ForeignKey("usuarioId")]
         public Usuarios Usuarios { get; set; }
