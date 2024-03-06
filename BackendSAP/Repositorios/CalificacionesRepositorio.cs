3818b2c9d35df86f4f1579953543e2753ec9e7d2
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-06 15:26:15 -0400

    Scores repositories implemented

--- BackendSAP/Repositorios/CalificacionesRepositorio.cs
+++ BackendSAP/Repositorios/CalificacionesRepositorio.cs
@@ -1,8 +1,54 @@
-﻿using BackendSAP.Repositorios.IRepositorios;
+﻿using BackendSAP.Data;
+using BackendSAP.Modelos;
+using BackendSAP.Repositorios.IRepositorios;
 
 namespace BackendSAP.Repositorios
 {
     public class CalificacionesRepositorio : ICalificacionesRepositorio
     {
+        private readonly ApplicationDbContext _bd;
+
+        public CalificacionesRepositorio(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+
+        public bool ActualizarCalificacion(Calificaciones calificacion)
+        {
+            _bd.Calificaciones.Update(calificacion);
+            return Guardar();
+        }
+
+        public bool BorrarCalificacion(Calificaciones calificacion)
+        {
+            _bd.Calificaciones.Remove(calificacion);
+            return Guardar();
+        }
+
+        public bool CrearCalificacion(Calificaciones calificacion)
+        {
+            _bd.Calificaciones.Add(calificacion);
+            return Guardar();
+        }
+
+        public bool ExisteCalificacion(int id)
+        {
+            return _bd.Estados.Any(c => c.Id == id);
+        }
+
+        public Calificaciones GetCalificacion(int calificacionId)
+        {
+            return _bd.Calificaciones.FirstOrDefault(c => c.Id == calificacionId);
+        }
+
+        public ICollection<Calificaciones> GetCalificaciones()
+        {
+            return _bd.Calificaciones.OrderBy(c => c.puntaje).ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _bd.SaveChanges() >= 0 ? true : false;
+        }
     }
 }
--- BackendSAP/Repositorios/IRepositorios/ICalificacionesRepositorio.cs
+++ BackendSAP/Repositorios/IRepositorios/ICalificacionesRepositorio.cs
@@ -1,6 +1,15 @@
-﻿namespace BackendSAP.Repositorios.IRepositorios
+﻿using BackendSAP.Modelos;
+
+namespace BackendSAP.Repositorios.IRepositorios
 {
     public interface ICalificacionesRepositorio
     {
+        ICollection<Calificaciones> GetCalificaciones();
+        Calificaciones GetCalificacion(int calificacionId);
+        bool ExisteCalificacion(int id);
+        bool CrearCalificacion(Calificaciones calificacion);
+        bool ActualizarCalificacion(Calificaciones calificacion);
+        bool BorrarCalificacion(Calificaciones calificacion);
+        bool Guardar();
     }
 }
