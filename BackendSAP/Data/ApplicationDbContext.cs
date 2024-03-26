fedab088a78f59dfd5a163388a509a9accff3dfd
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-26 18:29:29 -0400

    New migration (now with a check constraint in scores)

--- BackendSAP/Data/ApplicationDbContext.cs
+++ BackendSAP/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using BackendSAP.Modelos;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System.Reflection.Emit;
 
 namespace BackendSAP.Data
@@ -26,12 +27,13 @@ namespace BackendSAP.Data
             .OnDelete(DeleteBehavior.Restrict); // Especifica la acción en caso de eliminación
 
             builder.Entity<Calificaciones>()
-               .HasIndex(c => new { c.usuarioId, c.psicologoId })
-               .IsUnique();
+            .HasIndex(c => new { c.usuarioId, c.psicologoId })
+            .IsUnique();
 
             builder.Entity<Calificaciones>()
-                .HasCheckConstraint("CK_Usuario_Psicologo_Calificacion", "usuarioId <> psicologoId");
+            .HasCheckConstraint("CK_Usuario_Psicologo_Calificacion", "usuarioId <> psicologoId");
 
+            builder.Entity<Calificaciones>().ToTable(c => c.HasCheckConstraint("CK_puntaje", "[puntaje]  IN (1, 2, 3, 4, 5)"));
         }
 
         //Agregar los modelos aquí
---
+++ BackendSAP/Migrations/20240326183004_CambioDosTablaCalificaciones.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendSAP.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioDosTablaCalificaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_puntaje",
+                table: "Calificaciones",
+                sql: "[puntaje]  IN (1, 2, 3, 4, 5)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_puntaje",
+                table: "Calificaciones");
+        }
+    }
+}
