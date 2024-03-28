9850a0c1a8e48b6cf053e3a0cb8974cf9f201a48
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-28 19:36:09 -0400

    Score field in DTO added.

--- BackendSAP/Modelos/Dtos/Calificaciones/CalificacionesDto.cs
+++ BackendSAP/Modelos/Dtos/Calificaciones/CalificacionesDto.cs
@@ -6,6 +6,7 @@ namespace BackendSAP.Modelos.Dtos.Calificaciones
     {
         public int Id { get; set; }
         public string observacion { get; set; }
+        public int puntaje { get; set; }
         public string usuarioId { get; set; } // Clave Foránea del usuario quién hace la calificación
         public string psicologoId { get; set; } // Clave Foránea del usuario psicólogo quién recibe la calificación
     }
