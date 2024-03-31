b69762d03a660e725975bdbc94e6280179283e68
Andrés <95057542+andreselc@users.noreply.github.com>
2024-03-31 18:38:02 -0400

    New DTO implemented in user's model

---
+++ BackendSAP/Modelos/Dtos/Usuarios/UsuarioActualizarDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendSAP.Modelos.Dtos.Usuarios
+{
+    public class UsuarioActualizarDto
+    {
+        public string Id { get; set; }
+        public string? Email { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? TelefonOficina { get; set; }
+        /*public byte[]? ImagenPerfil { get; set; }*/
+        public int? CiudadId { get; set; } 
+
+    }
+}
